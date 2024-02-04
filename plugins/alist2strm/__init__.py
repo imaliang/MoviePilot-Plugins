021d8912c5fcf1a93ff0e9657d7e5d1e273144a9
imaliang <53564732+imaliang@users.noreply.github.com>
2024-02-04 09:38:24 +0000

    init

--- plugins/alist2strm/__init__.py
+++ plugins/alist2strm/__init__.py
@@ -58,7 +58,7 @@ class Alist2Strm(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/alist-org/docs/main/docs/.vuepress/public/logo.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "imaliang"
     # 作者主页
